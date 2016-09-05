bad51abbd9d35e64f5e025df758b9e12424c7733
ruijiao <1160676773@qq.com>
2016-09-05 17:08:06 +0800

    从屏幕底部滑出以显示更多内容的控件初始化

---
+++ app/src/main/java/com/demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 从屏幕底部滑出以显示更多内容的控件
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
